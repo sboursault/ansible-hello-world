273daa680fef148d7efe46108350ab3a638352b4
sboursault <sboursault@protonmail.com>
2018-10-24 13:13:56 +0200

    add block to mutualize become section on hello world tasks

---
+++ playbooks/roles/hello-world/files/start.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $HOME/config
+
+if pgrep -f "$HOME/app.py" >/dev/null 2>&1;
+then
+  echo "already running"
+else
+  nohup python $HOME/app.py &
+  sleep 2
+  if pgrep -f "$HOME/app.py" >/dev/null 2>&1;
+  then
+    echo "service started"
+  else
+    echo "failed to start"
+    exit 1
+  fi
+fi
